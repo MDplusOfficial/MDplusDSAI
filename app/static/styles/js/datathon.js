b74dadd60d5a0cb32788d1a2717689602d232e71
Michael Yao <michael.yao@pennmedicine.upenn.edu>
2023-10-12 10:57:38 -0400

    Added countdown timer

--- app/static/styles/js/datathon.js
+++ app/static/styles/js/datathon.js
@@ -1,3 +1,21 @@
+const countDownDate = new Date(Date.UTC(2023, 9, 25, 22, 0, 0)).getTime();
+function countDown() {
+  var now = new Date().getTime();
+  var distance = countDownDate - now;
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdown").style.display = "none";
+    document.getElementById("countdown-header").style.display = "none";
+  }
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  document.getElementById("countdown").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+}
+countDown();
+var x = setInterval(countDown, 1000);
+
 function consoleText(word, id) {
   var console = document.getElementById("console");
   console.style.opacity = "1";
